d28001c097a4efbb9a5c286f438c6c52ebd0b6a8
Gwhyyy <ffikhi.aanas@gmail.com>
2022-09-20 14:04:37 +0100

    [ Add ] emplemented the bookmarks header button pan effects

--- lib/app/modules/bookmarks/controllers/bookmarks_view_controller.dart
+++ lib/app/modules/bookmarks/controllers/bookmarks_view_controller.dart
@@ -1,6 +1,22 @@
+import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
-class BookmarksViewController extends GetxController {
+class BookmarksViewController extends GetxController
+    with HeaderButtonImplementationMixin {}
 
+mixin HeaderButtonImplementationMixin on GetxController {
+  final String idToUpdate = "headerButton";
+  Color? dynamicHeaderButtonBgColor;
+
+  void onTapDown(BuildContext context) {
+    dynamicHeaderButtonBgColor =
+        Theme.of(context).colorScheme.primary.withGreen(80);
+    update([idToUpdate]);
+  }
+
+  void onTapCancel() {
+    dynamicHeaderButtonBgColor = null;
+    update([idToUpdate]);
+  }
 }
--- lib/app/modules/bookmarks/view/widgets/medium_header_button.dart
+++ lib/app/modules/bookmarks/view/widgets/medium_header_button.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:medium_app_clone/app/modules/bookmarks/controllers/bookmarks_tabs_view_controller.dart';
+import 'package:medium_app_clone/app/modules/bookmarks/controllers/bookmarks_view_controller.dart';
 import 'package:medium_app_clone/helpers/string_extension/string_extension.dart';
 
 import '../../../../config/constants.dart';
 
-class MediumHeaderButton extends GetWidget<BookmarksTabsViewController> {
+class MediumHeaderButton extends GetWidget<BookmarksViewController> {
   const MediumHeaderButton({
     super.key,
     required this.text,
@@ -16,31 +17,42 @@ class MediumHeaderButton extends GetWidget<BookmarksTabsViewController> {
   final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      onPanDown: (details) {
-        // controller.onTapDown();
-      
+    return GetBuilder<BookmarksViewController>(
+      id: controller.idToUpdate,
+      builder: (controller) {
+        return GestureDetector(
+          onTap: onPressed,
+          onPanDown: (details) {
+            controller.onTapDown(context);
+          },
+          onPanCancel: () {
+            controller.onTapCancel();
+          },
+          onPanEnd: (details) {
+            controller.onTapCancel();
+          },
+          child: Container(
+            width: bookmarksHeaderButtonWidth,
+            height: bookmarksHeaderButtonHeight,
+            decoration: BoxDecoration(
+              color: controller.dynamicHeaderButtonBgColor ??
+                  Theme.of(context).buttonTheme.colorScheme!.primary,
+              borderRadius: BorderRadius.circular(
+                bookmarksHeaderButtonBorderRadius,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                text.firstLettersToCapital(),
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                      fontWeight: FontWeight.w100,
+                      color: Theme.of(context).primaryColor,
+                    ),
+              ),
+            ),
+          ),
+        );
       },
-      child: Container(
-        width: bookmarksHeaderButtonWidth,
-        height: bookmarksHeaderButtonHeight,
-        decoration: BoxDecoration(
-          color: Theme.of(context).buttonTheme.colorScheme!.primary,
-          borderRadius: BorderRadius.circular(
-            bookmarksHeaderButtonBorderRadius,
-          ),
-        ),
-        child: Center(
-          child: Text(
-            text.firstLettersToCapital(),
-            style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                  fontWeight: FontWeight.w100,
-                  color: dynamicColor ?? Theme.of(context).primaryColor,
-                ),
-          ),
-        ),
-      ),
     );
   }
 }
